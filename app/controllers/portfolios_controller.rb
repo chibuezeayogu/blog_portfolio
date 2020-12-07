b04f7f22e26f5eaaee0b836491eb816a721cec10
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-12-07 17:21:17 +0100

    implemented edit and update feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfilio_item, only: %i[show edit update destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -21,10 +23,29 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def show; end
+
+  def update
+    respond_to do |format|
+      if @portfolio_items.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: 'Record successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_items }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_itemsg.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
   def portfolio_item_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
+
+  def set_portfilio_item
+    @portfolio_items = Portfolio.find(params[:id])
   end
 end
