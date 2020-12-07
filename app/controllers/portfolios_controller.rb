796eb5f9c8e801f211d0c4575de43d71f2044669
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2020-12-07 15:39:16 +0100

    Revert "Integrated new and create actions for portfolios"

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,29 +2,4 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-
-  def new
-    @portfolio_item = Portfolio.new
-  end
-
-  def create
-    @portfolio_items = Portfolio.new(portfolio_item_params)
-
-    respond_to do |format|
-      if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio items was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_items }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-
-  private
-
-  def portfolio_item_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
-  end
 end
