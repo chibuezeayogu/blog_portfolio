0679b11172ff64756733ce7849a2cf7e1449de43
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-12-08 01:34:24 +0100

    Integrated friendly routes for blog post

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
---
+++ db/migrate/20201207234604_add_slug_to_blogs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddSlugToBlogs < ActiveRecord::Migration[6.0]
+  def change
+    add_column :blogs, :slug, :string
+    add_index :blogs, :slug, unique: true
+  end
+end
