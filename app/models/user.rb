2d296fecc126835aa7d8d67c0f408ea3fe059ccc
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-12-11 02:36:10 +0100

    Completed authentication with virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates :name, presence: true
+  def first_name
+    return '' if name.nil?
+
+    name.split.first
+  end
+
+  def last_name
+    return '' if name.nil?
+
+    name.split.last
+  end
 end
