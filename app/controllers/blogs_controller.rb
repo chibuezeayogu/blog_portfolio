9e1f7c688b4620ee371f1321d0585af75ec3ddba
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-12-16 00:26:14 +0100

    Implemented partials with local data for portfolio and nav bars

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy toggle_status]
   layout 'blog'
+  
   def index
     @blogs = Blog.all
     @page_title = 'My Portfolio Blog'
