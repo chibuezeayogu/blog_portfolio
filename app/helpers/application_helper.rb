74cd3e87db822e9a9f570713d3fdb43e9e6f286b
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-12-16 03:48:42 +0100

    Implemented the content tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(User)
@@ -6,6 +8,13 @@ module ApplicationHelper
       (link_to 'Register', new_user_registration_path) +
         '<br>'.html_safe +
         (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def source_helper
+    if session[:source]
+      greeting = "Thanks for visiting me from <#{session[:source]}"
+      content_tag(:p, greeting, class: 'source_greeting')
     end
   end
 end
